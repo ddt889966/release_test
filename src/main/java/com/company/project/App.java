24c6ef235e72a45bd89307f8149bcba27ff8815f
yuan ding <yd006t@att.com>
2017-03-16 13:35:56 -0400

    new change from develop branch

--- src/main/java/com/company/project/App.java
+++ src/main/java/com/company/project/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
 		System.out.println( "changes from release branch" );
+System.out.println( "changes from release branch" );
     }
 }
